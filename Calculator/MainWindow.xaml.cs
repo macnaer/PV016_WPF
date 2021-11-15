7b3d971b8d1a9c0f2b491e6d519a578b41adf3fd
trofimchuk <trofimchuk@itstep.academy>
2021-11-15 20:28:29 +0200

    Create additinal operator

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,14 +20,59 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        double lastNumber, res;
+        SelectedOperator selectedOperator;
+
         public MainWindow()
         {
             InitializeComponent();
+            result.Content = 0;
+        }
+
+        private void numbersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                selectedValue = int.Parse(btn.Content.ToString());
+            }
+
+            if (result.Content.ToString() == "0")
+            {
+                result.Content = $"{selectedValue}";
+            }
+            else
+            {
+                result.Content = $"{result.Content}{selectedValue}";
+            }
+        }
+
+        private void operatorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(result.Content.ToString(), out lastNumber))
+            {
+                result.Content = "0";
+            }
+
+            if (sender == plusBtn)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusBtn)
+                selectedOperator = SelectedOperator.Substruction;
+            if (sender == multiplyBtn)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divisionBtn)
+                selectedOperator = SelectedOperator.Divide;
         }
 
         private void acBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            result.Content = "0";
+            res = 0;
+            lastNumber = 0;
+
         }
 
         private void negativeBtn_Click(object sender, RoutedEventArgs e)
@@ -38,6 +83,43 @@ namespace Calculator
         private void percenageBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void pointBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void equalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(result.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        res = Calculate.Add(lastNumber, newNumber);
+                        break;
+                }
+
+                result.Content = res;
+            }
+        }
+
+        public enum SelectedOperator
+        {
+            Addition,
+            Substruction,
+            Multiplication,
+            Divide
+        }
+    }
+
+    public class Calculate
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
         }
     }
 }
