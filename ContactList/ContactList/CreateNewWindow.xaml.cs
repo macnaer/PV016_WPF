ccbbb864d10bf363f6eec05033352ea83ce8b1ec
k2 <trofimchuk.an@gmail.com>
2021-11-17 20:03:27 +0200

    Start new project contact list

---
+++ ContactList/ContactList/CreateNewWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using ContactList.Data.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactList
+{
+    /// <summary>
+    /// Interaction logic for CreateNewWindow.xaml
+    /// </summary>
+    public partial class CreateNewWindow : Window
+    {
+        public CreateNewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.dbPath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+                //connection.Update(contact);
+                //connection.Delete(contact);
+            }
+
+            Close();
+        }
+    }
+}
---
+++ ContactList/ContactList/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using ContactList.Data.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> contacts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            contacts = new List<Contact>();
+            GetContacts();
+
+        }
+
+        void GetContacts()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.dbPath))
+            {
+                connection.CreateTable<Contact>();
+                contacts = connection.Table<Contact>().ToList();
+
+                if(contacts != null)
+                {
+                    contactsListView.ItemsSource = contacts;
+                }
+            }
+        }
+
+        private void newContactBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewWindow createNewWindow = new CreateNewWindow();
+            createNewWindow.ShowDialog();
+            GetContacts();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
